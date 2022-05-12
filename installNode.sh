bdfa1f0df9afc87b8dce6e3b163108e14e0d5f35
matteac <mateoeestn2@gmail.com>
2022-05-12 11:04:24 -0300

    lll

--- installNode.sh
+++ installNode.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd /tmp
+
+#if you want to install other version change the link below
 wget https://nodejs.org/dist/v16.15.0/node-v16.15.0-linux-x64.tar.xz
 
 tar -xf node-v16.15.0-linux-x64.tar.xz
