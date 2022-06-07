8d21430bcab7322be974a6bb25759301f30d96d3
matteac <mateoeestn2@gmail.com>
2022-06-06 21:11:51 -0300

    sens added

---
+++ sens.sh
@@ -0,0 +1,9 @@
+xinput list
+
+echo -e "\nEnter device id:"
+read device
+
+echo -e "\nEnter the desired sensitivity (between -1 and 1):"
+read sens
+
+xinput --set-prop $device 'libinput Accel Speed' $sens
