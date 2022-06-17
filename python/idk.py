e164a1b0373ca9b5b0d71e9fda2042b5eb143dd0
matteac <mateoeestn2@gmail.com>
2022-06-17 17:26:22 -0300

    re-org

--- python/idk.py
+++ python/idk.py
@@ -2,38 +2,37 @@ import os
 import time
 
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    ENDC = '\033[0m'
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKCYAN = '\033[96m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+ENDC = '\033[0m'
 
 
 def check_input(data):
     try:
         if data == "":
             data = 1800
-            print(bcolors.OKGREEN + "Default time set to: 1800" + bcolors.ENDC)
+            print(OKGREEN + "Default time set to: 1800" + ENDC)
             return data
         else:
             data = int(data)
             return data
     except ValueError:
-        print(bcolors.FAIL + "\nPlease enter a number" + bcolors.ENDC)
+        print(FAIL + "\nPlease enter a number" + ENDC)
 
         main()
 
 
 def main():
     import pyautogui as gui
-    print(bcolors.OKGREEN + "How long do you want to sleep? (in seconds)" + bcolors.ENDC)
-    print(bcolors.WARNING + "3600 = 1 HOUR \n86400 = 1 DAY" + bcolors.ENDC)
-    print(bcolors.OKBLUE + bcolors.BOLD + "DEFAULT = 1800" + bcolors.ENDC)
+    print(OKGREEN + "How long do you want to sleep? (in seconds)" + ENDC)
+    print(WARNING + "3600 = 1 HOUR \n86400 = 1 DAY" + ENDC)
+    print(OKBLUE + BOLD + "DEFAULT = 1800" + ENDC)
     time_in_seconds = input()
     time_checked = check_input(time_in_seconds)
 
