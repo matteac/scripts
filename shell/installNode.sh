b81b0534d23dadb1c5158691b7459f06f82cc49a
matteac <mateoeestn2@gmail.com>
2022-06-25 09:55:25 -0300

    auto check latest LTS version

--- shell/installNode.sh
+++ shell/installNode.sh
@@ -3,14 +3,13 @@
 nodeVersion=$(curl https://nodejs.org/en/download/ | grep -m 1 'LTS')
 nodeVersion=${nodeVersion##*"LTS Version:"}
 nodeVersion=${nodeVersion##*"<strong>"}
-nodeVersion=${nodeVersion%%"</strong>"*}
-nodeVersion="v"$nodeVersion
+nodeVersion="v"${nodeVersion%%"</strong>"*}
 
 cd /tmp
 
 wget https://nodejs.org/dist/$nodeVersion/node-$nodeVersion-linux-x64.tar.xz
 
-tar -xf node-$nodeVersion-linux-x64.tar.xz
+tar -xf ./node-$nodeVersion-linux-x64.tar.xz
 sudo cp -r ./node-$nodeVersion-linux-x64/{bin,include,lib,share} /usr/
 
 sudo npm install npm@latest --location=global
